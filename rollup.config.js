7fe3121013baf0dc93bfc264b2466207377586f7
Ansoriddin0911 <ansoriddinabdunazarov@gmail.com>
2024-09-11 14:29:15 +0500

    updated files

--- rollup.config.js
+++ rollup.config.js
@@ -6,35 +6,13 @@ import { terser } from "rollup-plugin-terser";
 module.exports = {
   input: "src/FreeDraw.js",
   output: [
-    // {
-    //   file: "dist/leaflet-freedraw.cjs.js",
-    //   format: "cjs",
-    //   exports: "named",
-    //   sourcemap: true,
-    //   external: ["ramda", "leaflet", "clipper-lib"]
-    // },
-    // {
-    //   file: "dist/leaflet-freedraw.esm.js",
-    //   format: "esm",
-    //   sourcemap: true,
-    //   exports: "named",
-    //   external: ["ramda", "leaflet"]
-    // },
     {
       file: "dist/leaflet-freedraw.web.js",
       format: "cjs",
       sourcemap: true,
       exports: "named",
       external: ["ramda", "leaflet"]
-    },
-    // {
-    //   file: "dist/leaflet-freedraw.iife.js",
-    //   format: "iife",
-    //   sourcemap: true,
-    //   name: "LeafletFreeDraw",
-    //   exports: "named",
-    //   external: ["ramda", "leaflet"]
-    // }
+    }
   ],
   plugins: [
     resolve(),
